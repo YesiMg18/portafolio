995c92498f0acd6a0893dca464822715f3580495
Yesica Muñoz <yesicamg18@hotmail.com>
2023-10-03 12:19:50 -0600

    links projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,38 +20,38 @@ export const Projects = () =>{
                     <h1>PROYECTOS</h1>
                 </div>
                 <div className="projectsContainer">
-                    <div className="projectsCard">
+                    <a href="https://www.behance.net/gallery/154292977/Homepage-UI-Crehana-V3"><div className="projectsCard">
                         <img src={CrehanaHome}/>
                         <p>Rediseño de Homepage Crehana - V3</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/115663759/Ecommerce-Web-Design"><div className="projectsCard">
                         <img src={Eliya}/>
                         <p>Eccomerce Web Design - Eliya</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/133165985/UI-Landing-Design"><div className="projectsCard">
                         <img src={LandingCrehana}/>
                         <p>Landing Campaña Publicitaria</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/110111167/Mobile-app-Design"><div className="projectsCard">
                         <img src={Skrambla}/>
                         <p>App Iternario de Viajes Skrambla</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/140262243/Crehana-Homepage"><div className="projectsCard">
                         <img src={CrehanaHome2}/>
                         <p>Rediseño de Homepage Crehana - V3</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/108351317/App-Design-Travels"><div className="projectsCard">
                         <img src={Volindo}/>
                         <p>App de Reservaciones Volindo</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/113795609/Landing-Page-Design"><div className="projectsCard">
                         <img src={Landing}/>
                         <p>Landing Green Card</p>
-                    </div>
-                    <div className="projectsCard">
+                    </div></a>
+                    <a href="https://www.behance.net/gallery/119307241/WebSite-Design-UI-Product"><div className="projectsCard">
                         <img src={Machinery}/>
                         <p>Website Machinery Partner</p>
-                    </div>
+                    </div></a>
                 </div>
                 <div className="buttonContainer">
                     <a href='https://www.behance.net/yesicamuoz18'><img src={LogoBehance}/>Ver otros Proyectos</a>
